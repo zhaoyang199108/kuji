5405266f615e52e7939c4478574a35a6a128744c
zhaoyang <zhaoyang199108@163.com>
2016-11-17 14:54:55 +0800

    修改密码dao更改

--- kujisoftware/src/main/java/com/kuji/dao/UserDao.java
+++ kujisoftware/src/main/java/com/kuji/dao/UserDao.java
@@ -12,8 +12,9 @@ public interface UserDao {
 	 * 更新 账号  密码
 	 * @param model
 	 */
-	@Update("update user set upwd=#{mdl.upwd}, password=#{mdl.password}  "
-			+ "WHERE ID=#{mdl.id}")
-	public int updatePwd(@Param("mdl")User model);
+//	@Update("update user set upwd=#{mdl.upwd}, password=#{mdl.password}  "
+//			+ "WHERE ID=#{mdl.id}")
+//	public int updatePwd(@Param("user")User user);
+	public int updatePwd(User user);
 	
 }

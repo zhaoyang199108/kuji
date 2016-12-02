3bda9d651b4ca76ee34a2415bd6ce8c351a46376
Administrator <Administrator@ULOE3T20M9MZ3BS>
2016-12-02 11:34:06 +0800

    记忆训练修改删除查询

--- kujisoftware/src/main/java/com/kuji/service/MemoryTrainImpl/MemoryTrainImpl.java
+++ kujisoftware/src/main/java/com/kuji/service/MemoryTrainImpl/MemoryTrainImpl.java
@@ -1,5 +1,7 @@
 package com.kuji.service.MemoryTrainImpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,15 +17,51 @@ public class MemoryTrainImpl  implements MemoryTrainService{
 	public int insertIntoMemoryTrain(MemoryTrain memoryTrain) {
 		int count = memoryTrainDao.insertIntoMemoryTrain(memoryTrain);
 		if(count>0){
-			return 0;
-		}else{
 			return 1;
+		}else{
+			return 0;
 		}
 	}
 
 	public MemoryTrain findMemoryTrainByExerciseAndWhichDayAndType(
 			MemoryTrain memoryTrain) {
 		return memoryTrainDao.findMemoryTrainByExerciseAndWhichDayAndType(memoryTrain);
+	}
+
+	public List<MemoryTrain> findAll() {
+		return memoryTrainDao.findAll();
+	}
+
+	public MemoryTrain findMemoryTrainById(long id) {
+		return memoryTrainDao.findMemoryTrainById(id);
+	}
+
+	public int deleteMemoryTrainById(long id) {
+		return memoryTrainDao.deleteMemoryTrainById(id);
+	}
+
+	public int query(String memoryTrainType,
+			String memoryTrainWhichDay, String exerciseId) {
+		int count = memoryTrainDao.query(memoryTrainType,memoryTrainWhichDay,exerciseId);
+		if(count > 0){
+            return 0;
+        }else{
+            return 1;
+        }
+	}
+
+	public int updateMemoryTrain(MemoryTrain memoryTrain) {
+		try{
+			int count = memoryTrainDao.updateMemoryTrain(memoryTrain);
+			if(count>0){
+				return 1;
+			}else{
+				return 0;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return 0;
 	}
 
 }
--- kujisoftware/src/main/java/com/kuji/service/MemoryTrainService.java
+++ kujisoftware/src/main/java/com/kuji/service/MemoryTrainService.java
@@ -1,5 +1,7 @@
 package com.kuji.service;
 
+import java.util.List;
+
 import com.kuji.entity.MemoryTrain;
 
 public interface MemoryTrainService {
@@ -7,5 +9,15 @@ public interface MemoryTrainService {
 	int   insertIntoMemoryTrain(MemoryTrain memoryTrain);//添加
 
 	MemoryTrain findMemoryTrainByExerciseAndWhichDayAndType(MemoryTrain memoryTrain);
+
+	List<MemoryTrain> findAll();
+
+	MemoryTrain findMemoryTrainById(long id);
+
+	int deleteMemoryTrainById(long id);
+
+	int query(String memoryTrainType, String memoryTrainWhichDay,String exerciseId);
+
+	int updateMemoryTrain(MemoryTrain memoryTrain);
 	
 }
